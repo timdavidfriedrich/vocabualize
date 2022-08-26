85f67205ca36464255c2ad7e951886b8419cea7c
Tim Friedrich <tim.david007@gmail.com>
2022-08-26 20:11:19 +0200

    practise mode first ui

---
+++ lib/screens/practise.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Practise extends StatefulWidget {
+  const Practise({Key? key}) : super(key: key);
+
+  @override
+  State<Practise> createState() => _PractiseState();
+}
+
+class _PractiseState extends State<Practise> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("13/37")),
+    );
+  }
+}
---
+++ lib/screens/practise_done.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vocabualize/screens/home.dart';
+import 'package:vocabualize/utils/teleport.dart';
+
+class PractiseDone extends StatelessWidget {
+  const PractiseDone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(48, 0, 48, 0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    children: [
+                      TextSpan(
+                        text: "Congratulations!\n",
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const TextSpan(
+                          text: "You finished all of your current words!"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushReplacement(
+                context,
+                Teleport(child: const Home(), type: "fade"),
+              ),
+              style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).colorScheme.surface,
+                  onPrimary: Theme.of(context).colorScheme.onSurface),
+              child: const Text("Main menu"),
+            ),
+            const SizedBox(height: 64),
+          ],
+        ),
+      ),
+    );
+  }
+}
