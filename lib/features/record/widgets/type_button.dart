ef9a165c6a120c5add45dac52fd0efbd5909335c
Tim David Friedrich <timdavidfriedrich@gmail.com>
2022-09-20 18:02:40 +0200

    editDialog is done (maybe)

--- lib/features/record/widgets/type_button.dart
+++ lib/features/record/widgets/type_button.dart
@@ -17,66 +17,107 @@ class _TypeButtonState extends State<TypeButton> {
   TextEditingController controller = TextEditingController();
   String currentSource = "";
 
+  _focus() {
+    Provider.of<ActiveProvider>(context, listen: false).typeIsActive = true;
+  }
+
+  _cancel() {
+    FocusManager.instance.primaryFocus?.unfocus();
+    controller.clear();
+    currentSource = "";
+    Provider.of<ActiveProvider>(context, listen: false).typeIsActive = false;
+  }
+
+  _submit() async {
+    Messenger.loadingAnimation();
+    Vocabulary newVocabulary = Vocabulary(source: currentSource, target: await Translator.translate(currentSource));
+    if (!mounted) return;
+    Provider.of<VocabularyProvider>(context, listen: false).add(newVocabulary).whenComplete(() {
+      Navigator.pop(context);
+      Messenger.showSaveMessage(newVocabulary);
+    });
+    currentSource = "";
+    controller.clear();
+    Provider.of<ActiveProvider>(context, listen: false).typeIsActive = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Expanded(
-          child: TextFormField(
-            style: Theme.of(context).textTheme.bodyMedium,
-            toolbarOptions: const ToolbarOptions(copy: false, cut: false, paste: false, selectAll: false),
-            autofocus: false,
-            controller: controller,
-            textInputAction: TextInputAction.done,
-            maxLength: 20,
-            maxLines: 1,
-            //onTapOutside: (event) => FocusManager.instance.primaryFocus?.unfocus(),
-            decoration: InputDecoration(
-              enabled: !Provider.of<ActiveProvider>(context).micIsActive,
-              hintText: "Type instead ...",
-              counterText: "",
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary, width: 4),
-                  borderRadius: BorderRadius.circular(16)),
-              border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary, width: 4),
-                  borderRadius: BorderRadius.circular(16)),
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary, width: 4),
-                  borderRadius: BorderRadius.circular(16)),
+        TextFormField(
+          style: Theme.of(context).textTheme.bodyMedium,
+          toolbarOptions: const ToolbarOptions(copy: false, cut: false, paste: false, selectAll: false),
+          autofocus: false,
+          controller: controller,
+          textInputAction: TextInputAction.done,
+          maxLength: 20,
+          maxLines: 1,
+          onTapOutside: (event) => FocusManager.instance.primaryFocus?.unfocus(),
+          /*
+            textSelectionTheme: TextSelectionThemeData(
+              cursorColor: surface,
+              selectionColor: hint,
+              selectionHandleColor: surface,
             ),
-            onTap: Provider.of<ActiveProvider>(context).micIsActive
-                ? () {}
-                : () => Provider.of<ActiveProvider>(context, listen: false).typeIsActive = true,
-            onChanged: (text) => setState(() {
-              currentSource = text;
-              Provider.of<ActiveProvider>(context, listen: false).typeIsActive = true;
-            }),
-            onFieldSubmitted: (text) async {
-              Messenger.loadingAnimation();
-              Vocabulary newVocabulary = Vocabulary(source: currentSource, target: await Translator.translate(currentSource));
-              if (!mounted) return;
-              Provider.of<VocabularyProvider>(context, listen: false).add(newVocabulary).whenComplete(() {
-                Navigator.pop(context);
-                Messenger.showSaveMessage(newVocabulary);
-              });
-              currentSource = "";
-              controller.clear();
-              Provider.of<ActiveProvider>(context, listen: false).typeIsActive = false;
-            },
+            inputDecorationTheme: InputDecorationTheme(
+              hintStyle: TextStyle(color: onPrimary),
+              labelStyle: TextStyle(color: onPrimary),
+              alignLabelWithHint: true,
+              filled: false,
+              contentPadding: const EdgeInsets.fromLTRB(18, 18, 18, 18),
+              enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: onBackground, width: 4), borderRadius: BorderRadius.circular(16)),
+              disabledBorder: OutlineInputBorder(borderSide: BorderSide(color: hint, width: 4), borderRadius: BorderRadius.circular(16)),
+              border: OutlineInputBorder(borderSide: BorderSide(color: onBackground, width: 4), borderRadius: BorderRadius.circular(16)),
+              focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: onBackground, width: 4), borderRadius: BorderRadius.circular(16)),
+              errorBorder: OutlineInputBorder(borderSide: BorderSide(color: error), borderRadius: BorderRadius.circular(16)),
+            ),
+          */
+          decoration: InputDecoration(
+            enabled: !Provider.of<ActiveProvider>(context).micIsActive,
+            hintText: "Type instead ...",
+            counterText: "",
+            contentPadding: const EdgeInsets.fromLTRB(18, 18, 18, 18),
+            hintStyle: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+            labelStyle: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary, width: 4), borderRadius: BorderRadius.circular(16)),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary, width: 4), borderRadius: BorderRadius.circular(16)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary, width: 4), borderRadius: BorderRadius.circular(16)),
           ),
+          onTap: Provider.of<ActiveProvider>(context).micIsActive ? () {} : _focus,
+          onChanged: (text) {
+            setState(() => currentSource = text);
+            _focus(); // long pressing on field ignores onTap, that's why this workaround
+          },
+          onFieldSubmitted: (text) async => _submit,
         ),
+        currentSource.isEmpty && !Provider.of<ActiveProvider>(context).typeIsActive ? Container() : const SizedBox(height: 16),
         currentSource.isEmpty && !Provider.of<ActiveProvider>(context).typeIsActive
             ? Container()
-            : IconButton(
-                onPressed: () {
-                  FocusManager.instance.primaryFocus?.unfocus();
-                  controller.clear();
-                  currentSource = "";
-                  Provider.of<ActiveProvider>(context, listen: false).typeIsActive = false;
-                },
-                icon: const Icon(Icons.clear_rounded),
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(side: BorderSide(width: 3, color: Theme.of(context).colorScheme.onPrimary)),
+                    onPressed: _cancel,
+                    child: const Text("Close"),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Theme.of(context).colorScheme.primary,
+                        backgroundColor: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                      onPressed: _submit,
+                      child: const Text("Add"),
+                    ),
+                  ),
+                ],
               ),
       ],
     );
