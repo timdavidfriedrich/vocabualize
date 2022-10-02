b83e6f0ae089e8fd827fe80074836142459c0cad
Tim David Friedrich <timdavidfriedrich@gmail.com>
2022-10-02 23:20:57 +0200

    TTS uses choosen language now

--- lib/features/core/services/text_to_speech.dart
+++ lib/features/core/services/text_to_speech.dart
@@ -1,12 +1,13 @@
+import 'package:provider/provider.dart';
 import 'package:text_to_speech/text_to_speech.dart';
+import 'package:vocabualize/constants/keys.dart';
+import 'package:vocabualize/features/settings/providers/settings_provider.dart';
 
 class TTS {
   static final TextToSpeech _tts = TextToSpeech();
 
   static speak(String text) async {
-    // List<String> languages = await _tts.getLanguages();
-    // Log.hint(languages);
-    _tts.setLanguage("es-ES"); // TODO: make dynamic
+    _tts.setLanguage(Provider.of<SettingsProvider>(Keys.context, listen: false).targetLanguage.textToSpeechId);
     _tts.speak(text);
   }
 }
