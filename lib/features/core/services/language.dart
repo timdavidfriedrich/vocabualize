0e6c2cf07d6d0707f62f04ea40ae02e4e7cc259b
Tim <timdavidfriedrich@gmail.com>
2023-02-01 22:44:05 +0100

    comment

--- lib/features/core/services/language.dart
+++ lib/features/core/services/language.dart
@@ -2,6 +2,7 @@ class Language {
   Language({required this.name, required this.translatorId, required this.speechToTextId, required this.textToSpeechId});
 
   /// TODO: apply device language for name (to have the correct language on init)
+  /// => Maybe setting this at onboarding will be enough
   factory Language.defaultSource() => Language(name: "English", translatorId: "en", speechToTextId: "en_AU", textToSpeechId: "en-US");
   factory Language.defaultTarget() => Language(name: "Spanish", translatorId: "es", speechToTextId: "es_AR", textToSpeechId: "es-ES");
 
