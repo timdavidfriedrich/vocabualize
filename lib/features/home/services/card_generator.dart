31811096b5774505980b3340978993d1d553ecef
Tim <timdavidfriedrich@gmail.com>
2023-02-01 18:53:25 +0100

    new card info generator system

--- lib/features/home/services/card_generator.dart
+++ lib/features/home/services/card_generator.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:log/log.dart';
 import 'package:provider/provider.dart';
 import 'package:vocabualize/constants/global.dart';
 import 'package:vocabualize/features/core/providers/vocabulary_provider.dart';
@@ -9,10 +12,22 @@ class CardGenerator {
     List<Vocabulary> vocabularyList = Provider.of<VocabularyProvider>(Global.context, listen: false).vocabularyList;
     List<Vocabulary> createdToday = Provider.of<VocabularyProvider>(Global.context, listen: false).createdToday;
 
-    if (vocabularyList.isEmpty) return AppLocalizations.of(Global.context).home_statusCard_isEmpty;
-    if (createdToday.length >= 3) return AppLocalizations.of(Global.context).home_statusCard_addedToday(createdToday.length);
-    if (vocabularyList.length >= 10) return AppLocalizations.of(Global.context).home_statusCard_addedManyInTotal(vocabularyList.length);
-    if (vocabularyList.length == 1) return AppLocalizations.of(Global.context).home_statusCard_onlyOneWord(vocabularyList.length);
-    return AppLocalizations.of(Global.context).home_statusCard_default(vocabularyList.length);
+    List<String> possibleInfos = [];
+
+    if (vocabularyList.isEmpty) {
+      possibleInfos.add(AppLocalizations.of(Global.context).home_statusCard_isEmpty);
+    }
+    if (createdToday.length >= 3) {
+      possibleInfos.add(AppLocalizations.of(Global.context).home_statusCard_addedToday(createdToday.length));
+    }
+    if (vocabularyList.length >= 10) {
+      possibleInfos.add(AppLocalizations.of(Global.context).home_statusCard_addedManyInTotal(vocabularyList.length));
+    }
+    if (vocabularyList.length == 1) {
+      possibleInfos.add(AppLocalizations.of(Global.context).home_statusCard_onlyOneWord(vocabularyList.length));
+    }
+    possibleInfos.add(AppLocalizations.of(Global.context).home_statusCard_default(vocabularyList.length));
+
+    return possibleInfos.elementAt(Random().nextInt(possibleInfos.length));
   }
 }
