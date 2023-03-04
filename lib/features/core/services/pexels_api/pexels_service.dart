b248674cd47b2d7b2c0e5fafded2473262098d6f
Tim <timdavidfriedrich@gmail.com>
2023-03-04 10:47:08 +0100

    moved pexels secret

--- lib/features/core/services/pexels_api/pexels_service.dart
+++ lib/features/core/services/pexels_api/pexels_service.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:http/http.dart';
 import 'package:vocabualize/features/core/services/pexels_api/pexels_model.dart';
-import 'package:vocabualize/features/core/services/pexels_api/pexels_key.dart';
+import 'package:vocabualize/constants/secrets/pexels_secret.dart';
 
 class PexelsService {
   Future<List<PexelsModel>> getImages(String search) async {
@@ -11,7 +11,7 @@ class PexelsService {
     Client client = Client();
     Response response = await client.get(
       Uri.parse("https://api.pexels.com/v1/search?query=$search&per_page=70"),
-      headers: {HttpHeaders.authorizationHeader: pexelsKey},
+      headers: {HttpHeaders.authorizationHeader: PexelsSecret.pexels},
     );
     if (response.statusCode == 200) {
       dynamic decoded = json.decode(response.body)["photos"];
