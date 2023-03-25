7dd741b98ef169950d33b16f5a8684fbe26ec38c
Tim <timdavidfriedrich@gmail.com>
2023-03-25 12:00:43 +0100

    practise progress bar redesign (not final?)

--- lib/features/practise/screens/practise_screen.dart
+++ lib/features/practise/screens/practise_screen.dart
@@ -39,23 +39,31 @@ class _PractiseScreenState extends State<PractiseScreen> {
     setState(() => isSolutionShown = false);
     if (vocabulariesToPractise.isNotEmpty) {
       setState(() {
-        if (currentVoc.source.isNotEmpty) vocabulariesToPractise.remove(currentVoc);
-        if (vocabulariesToPractise.isNotEmpty) currentVoc = vocabulariesToPractise.first;
+        if (currentVoc.source.isNotEmpty)
+          vocabulariesToPractise.remove(currentVoc);
+        if (vocabulariesToPractise.isNotEmpty)
+          currentVoc = vocabulariesToPractise.first;
       });
     } else {
       setState(() => isDone = true);
     }
   }
 
+  void _close() {
+    Navigator.pop(context);
+  }
+
   @override
   void initState() {
     super.initState();
     SchedulerBinding.instance.addPostFrameCallback((_) {
-      PractiseScreenArguments arguments = ModalRoute.of(context)!.settings.arguments as PractiseScreenArguments;
+      PractiseScreenArguments arguments =
+          ModalRoute.of(context)!.settings.arguments as PractiseScreenArguments;
       setState(() {
         vocabulariesToPractise = arguments.vocabulariesToPractise;
         initialVocCount = vocabulariesToPractise.length;
-        isMultilingual = Provider.of<VocabularyProvider>(context, listen: false).isMultilingual;
+        isMultilingual = Provider.of<VocabularyProvider>(context, listen: false)
+            .isMultilingual;
       });
       if (vocabulariesToPractise.isNotEmpty) _refreshVoc();
     });
@@ -67,39 +75,62 @@ class _PractiseScreenState extends State<PractiseScreen> {
         ? const PractiseDoneScreen()
         : SafeArea(
             child: ClipRRect(
-              borderRadius: const BorderRadius.only(topLeft: Radius.circular(32), topRight: Radius.circular(32)),
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(32), topRight: Radius.circular(32)),
               child: Scaffold(
-                // ? Show back button? (if not, remove AppBar)
-                appBar: AppBar(),
                 body: Padding(
                   padding: const EdgeInsets.fromLTRB(48, 0, 48, 0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      // const SizedBox(height: 48),
-                      Text(AppLocalizations.of(context).practise_left(vocabulariesToPractise.length),
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayMedium!
-                              .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold)),
-                      const SizedBox(height: 12),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(6),
-                        child: LinearProgressIndicator(
-                          value: vocabulariesToPractise.isEmpty ? 1 : 1 - (vocabulariesToPractise.length / initialVocCount),
-                          minHeight: 12,
-                          color: Theme.of(context).colorScheme.primary,
-                          backgroundColor: Theme.of(context).colorScheme.surface,
-                        ),
+                      const SizedBox(height: 36),
+                      Row(
+                        children: [
+                          Text(
+                              "${initialVocCount - vocabulariesToPractise.length} / $initialVocCount",
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displayMedium!
+                                  .copyWith(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      fontWeight: FontWeight.bold)),
+                                      const SizedBox(width: 24),
+                          Expanded(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(6),
+                              child: LinearProgressIndicator(
+                                value: vocabulariesToPractise.isEmpty
+                                    ? 1
+                                    : 1 -
+                                        (vocabulariesToPractise.length /
+                                            initialVocCount),
+                                minHeight: 12,
+                                color: Theme.of(context).colorScheme.primary,
+                                backgroundColor:
+                                    Theme.of(context).colorScheme.surface,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 16),
+                          IconButton(padding: EdgeInsets.zero, onPressed: () => _close(), icon: const Icon(Icons.close_rounded, weight: 4)),
+                        ],
                       ),
                       const Spacer(),
                       !isMultilingual
                           ? Container()
-                          : Text("${currentVoc.sourceLanguage.name}  ►  ${currentVoc.targetLanguage.name}",
-                              style: TextStyle(color: Theme.of(context).hintColor), textAlign: TextAlign.center),
-                      !isMultilingual ? Container() : const SizedBox(height: 12),
-                      Provider.of<SettingsProvider>(context).areImagesDisabled && !isSolutionShown
+                          : Text(
+                              "${currentVoc.sourceLanguage.name}  ►  ${currentVoc.targetLanguage.name}",
+                              style:
+                                  TextStyle(color: Theme.of(context).hintColor),
+                              textAlign: TextAlign.center),
+                      !isMultilingual
+                          ? Container()
+                          : const SizedBox(height: 12),
+                      Provider.of<SettingsProvider>(context)
+                                  .areImagesDisabled &&
+                              !isSolutionShown
                           ? Container()
                           : Expanded(
                               flex: 2,
@@ -107,16 +138,23 @@ class _PractiseScreenState extends State<PractiseScreen> {
                                 decoration: BoxDecoration(
                                   color: Theme.of(context).colorScheme.surface,
                                   borderRadius: BorderRadius.circular(24),
-                                  image: Provider.of<SettingsProvider>(context).areImagesDisabled
+                                  image: Provider.of<SettingsProvider>(context)
+                                          .areImagesDisabled
                                       ? null
-                                      : DecorationImage(fit: BoxFit.cover, image: currentVoc.imageProvider),
+                                      : DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: currentVoc.imageProvider),
                                 ),
                                 child: !isSolutionShown
                                     ? null
                                     : Container(
                                         decoration: BoxDecoration(
-                                          color: Theme.of(context).colorScheme.surface.withOpacity(0.75),
-                                          borderRadius: BorderRadius.circular(24),
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .surface
+                                              .withOpacity(0.75),
+                                          borderRadius:
+                                              BorderRadius.circular(24),
                                         ),
                                         child: Center(
                                             child: Row(
@@ -125,19 +163,27 @@ class _PractiseScreenState extends State<PractiseScreen> {
                                             Flexible(
                                               child: Text(
                                                 currentVoc.target,
-                                                style: Theme.of(context).textTheme.headlineMedium,
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headlineMedium,
                                                 textAlign: TextAlign.center,
                                               ),
                                             ),
                                             const SizedBox(width: 8),
-                                            IconButton(onPressed: () => _speak(), icon: const Icon(Icons.volume_up_rounded, size: 32)),
+                                            IconButton(
+                                                onPressed: () => _speak(),
+                                                icon: const Icon(
+                                                    Icons.volume_up_rounded,
+                                                    size: 32)),
                                           ],
                                         )),
                                       ),
                               ),
                             ),
                       const SizedBox(height: 32),
-                      Center(child: Text(currentVoc.source, style: Theme.of(context).textTheme.bodyMedium)),
+                      Center(
+                          child: Text(currentVoc.source,
+                              style: Theme.of(context).textTheme.bodyMedium)),
                       const Spacer(),
                       !isSolutionShown
                           ? Container()
@@ -147,42 +193,48 @@ class _PractiseScreenState extends State<PractiseScreen> {
                                 Expanded(
                                   child: ElevatedButton(
                                     style: ElevatedButton.styleFrom(
-                                      padding: const EdgeInsets.fromLTRB(0, 12, 0, 12),
+                                      padding: const EdgeInsets.fromLTRB(
+                                          0, 12, 0, 12),
                                       backgroundColor: LevelPalette.beginner,
                                     ),
                                     onPressed: () async {
                                       await currentVoc.answer(Answer.hard);
                                       _refreshVoc();
                                     },
-                                    child: Text(AppLocalizations.of(context).pracise_rating_hardButton),
+                                    child: Text(AppLocalizations.of(context)
+                                        .pracise_rating_hardButton),
                                   ),
                                 ),
                                 const SizedBox(width: 16),
                                 Expanded(
                                   child: ElevatedButton(
                                     style: ElevatedButton.styleFrom(
-                                      padding: const EdgeInsets.fromLTRB(0, 12, 0, 12),
+                                      padding: const EdgeInsets.fromLTRB(
+                                          0, 12, 0, 12),
                                       backgroundColor: LevelPalette.advanced,
                                     ),
                                     onPressed: () async {
                                       await currentVoc.answer(Answer.good);
                                       _refreshVoc();
                                     },
-                                    child: Text(AppLocalizations.of(context).pracise_rating_goodButton),
+                                    child: Text(AppLocalizations.of(context)
+                                        .pracise_rating_goodButton),
                                   ),
                                 ),
                                 const SizedBox(width: 16),
                                 Expanded(
                                   child: ElevatedButton(
                                     style: ElevatedButton.styleFrom(
-                                      padding: const EdgeInsets.fromLTRB(0, 12, 0, 12),
+                                      padding: const EdgeInsets.fromLTRB(
+                                          0, 12, 0, 12),
                                       backgroundColor: LevelPalette.expert,
                                     ),
                                     onPressed: () async {
                                       await currentVoc.answer(Answer.easy);
                                       _refreshVoc();
                                     },
-                                    child: Text(AppLocalizations.of(context).pracise_rating_easyButton),
+                                    child: Text(AppLocalizations.of(context)
+                                        .pracise_rating_easyButton),
                                   ),
                                 ),
                               ],
@@ -190,8 +242,10 @@ class _PractiseScreenState extends State<PractiseScreen> {
                       const SizedBox(height: 16),
                       !isSolutionShown
                           ? ElevatedButton(
-                              onPressed: () => setState(() => isSolutionShown = true),
-                              child: Text(AppLocalizations.of(context).pracise_solutionButton),
+                              onPressed: () =>
+                                  setState(() => isSolutionShown = true),
+                              child: Text(AppLocalizations.of(context)
+                                  .pracise_solutionButton),
                             )
                           : OutlinedButton(
                               onPressed: () async {
@@ -199,8 +253,10 @@ class _PractiseScreenState extends State<PractiseScreen> {
                                 _refreshVoc();
                               },
                               child: Text(
-                                AppLocalizations.of(context).pracise_rating_didntKnowButton,
-                                style: const TextStyle(color: LevelPalette.novice),
+                                AppLocalizations.of(context)
+                                    .pracise_rating_didntKnowButton,
+                                style:
+                                    const TextStyle(color: LevelPalette.novice),
                               ),
                             ),
                       const SizedBox(height: 64),
