f04c8d0a723f9031e09e12a864b4f44fb9ac6e7a
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2023-11-09 11:22:03 +0100

    [CHANGE] AuthService to use pocketbase

--- lib/features/core/services/auth_service.dart
+++ lib/features/core/services/auth_service.dart
@@ -1,81 +1,99 @@
+import 'dart:async';
+
+import 'package:pocketbase/pocketbase.dart';
+import 'package:secure_shared_preferences/secure_shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vocabualize/constants/common_imports.dart';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:log/log.dart';
-import 'package:vocabualize/features/core/models/app_user.dart';
-import 'package:vocabualize/features/core/services/data/cloud_service.dart';
 import 'package:vocabualize/features/core/services/messaging_service.dart';
-import 'package:vocabualize/features/onboarding/widgets/password_reset_failed_dialog.dart';
-import 'package:vocabualize/features/onboarding/widgets/sign_in_failed_dialog.dart';
-import 'package:vocabualize/features/onboarding/widgets/sign_up_failed_dialog.dart';
+import 'package:vocabualize/features/core/services/pocketbase_connection.dart';
 
 class AuthService {
-  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  static final User? user = FirebaseAuth.instance.currentUser;
+  static final AuthService instance = AuthService();
 
-  static Future<void> reloadUser() async {
-    await user!.reload();
+  final PocketBase _pocketbase = PocketbaseConnection.instance.pocketbase;
+  final String _usersCollectionName = "users";
+
+  SharedPreferences? _sharedPreferences;
+  SecureSharedPref? _secureSharedPreferences;
+
+  AuthStore get authStore => _pocketbase.authStore;
+  Stream<AuthStoreEvent> get authStream => _pocketbase.authStore.onChange;
+
+  AuthService() {
+    _loadLocalAuthStore();
   }
 
-  static Future signInAnonymously() async {
-    MessangingService.loadingAnimation();
-    try {
-      await _firebaseAuth.signInAnonymously();
-    } catch (error) {
-      Log.error(error);
-    }
-    Navigator.pop(Global.context);
+  Future<void> _loadLocalAuthStore() async {
+    _secureSharedPreferences = await SecureSharedPref.getInstance();
+    AuthStore localAuthStore = AsyncAuthStore(
+      save: (String data) async => _secureSharedPreferences?.putString('authStore', data, isEncrypted: true),
+      initial: await _secureSharedPreferences?.getString('authStore', isEncrypted: true),
+    );
+    Log.hint("Loadeded AuthStore from SecureSharedPreferences: $localAuthStore");
+    authStore.save(localAuthStore.token, localAuthStore.model);
   }
 
-  static Future createUserWithEmailAndPassword(String email, String password) async {
-    MessangingService.loadingAnimation();
-    try {
-      await _firebaseAuth.createUserWithEmailAndPassword(email: email.trim(), password: password);
-      await sendVerificationEmail();
-      Navigator.pop(Global.context);
-    } on FirebaseAuthException catch (error) {
-      MessangingService.showStaticDialog(SignUpFailedDialog(error: error));
-      Log.error(error);
-    }
+  Future signInAnonymously() async {
+    // ? TODO: Implement signInAnonymously ?
   }
 
-  static Future sendVerificationEmail() async {
+  Future<bool> signInWithEmailAndPassword(String email, String password) async {
     try {
-      await user!.sendEmailVerification();
-    } catch (error) {
-      Log.error(error);
-    }
-  }
-
-  static Future signInWithEmailAndPassword(String email, String password) async {
-    MessangingService.loadingAnimation();
-    try {
-      await _firebaseAuth.signInWithEmailAndPassword(email: email.trim(), password: password);
-      await CloudService.instance.loadUserData();
-      Navigator.pop(Global.context);
-    } on FirebaseAuthException catch (error) {
-      MessangingService.showStaticDialog(SignInFailedDialog(error: error));
-      Log.error(error);
+      final RecordAuth authData = await _pocketbase.collection(_usersCollectionName).authWithPassword(email, password);
+      authStore.save(authData.token, authData.record);
+      Log.hint("Signed in with email and password (AuthData: $authData)");
+      return true;
+    } catch (e) {
+      MessangingService.showStaticDialog(AlertDialog.adaptive(content: Text(e.toString())));
+      Log.error("Could not sign in with passwort: $e");
+      return false;
     }
   }
 
-  static Future signOut() async {
-    MessangingService.loadingAnimation();
+  Future<bool> createUserWithEmailAndPassword(String email, String password) async {
     try {
-      await _firebaseAuth.signOut();
-      await AppUser.instance.signOut();
-    } catch (error) {
-      Log.error(error);
+      final RecordModel authData = await _pocketbase.collection(_usersCollectionName).create(body: {
+        "email": email,
+        "password": password,
+      });
+      Log.hint("Created user with email and password (AuthData: $authData)");
+      return true;
+    } catch (e) {
+      MessangingService.showStaticDialog(AlertDialog.adaptive(content: Text(e.toString())));
+      Log.error("Could not create user with email and password: $e");
+      return false;
     }
-    Navigator.pop(Global.context);
   }
 
-  static Future<void> sendPasswordResetEmail(String email) async {
+  Future sendVerificationEmail() async {
+    // TODO: Implement sendVerificationEmail
+  }
+
+  Future<void> sendPasswordResetEmail(String email) async {
+    // TODO: Implement sendPasswordResetEmail
+  }
+
+  Future<bool> signOut() async {
     try {
-      await _firebaseAuth.sendPasswordResetEmail(email: email);
-    } on FirebaseAuthException catch (error) {
-      MessangingService.showStaticDialog(PasswordResetFailedDialog(error: error));
-      Log.error(error);
+      await _resetUserData();
+      await _resetAuthStore();
+      return true;
+    } catch (e) {
+      Log.error("Could not sign out: $e");
+      return false;
     }
+  }
+
+  Future<void> _resetAuthStore() async {
+    _secureSharedPreferences = await SecureSharedPref.getInstance();
+    await _secureSharedPreferences?.putString('authStore', "", isEncrypted: true);
+    authStore.clear();
+  }
+
+  Future<void> _resetUserData() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    await _sharedPreferences?.setString('user', "");
   }
 }
