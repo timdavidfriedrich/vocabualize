9b680f367efb9b0050ed767c767820c7e9724c5e
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2023-11-09 12:16:54 +0100

    [CHANGE] root StreamController to listen for user

--- lib/features/core/widgets/root.dart
+++ lib/features/core/widgets/root.dart
@@ -1,5 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:vocabualize/constants/common_imports.dart';
+import 'package:vocabualize/features/core/models/app_user.dart';
+import 'package:vocabualize/features/core/services/data/cloud_service.dart';
+import 'package:vocabualize/features/core/streams/user_stream.dart';
 import 'package:vocabualize/features/home/screens/home_screen.dart';
 import 'package:vocabualize/features/onboarding/screens/verify_screen.dart';
 import 'package:vocabualize/features/onboarding/screens/welcome_screen.dart';
@@ -10,19 +12,20 @@ class Root extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      // stream: Provider.of<AuthProvider>(context).userChanges,
-      stream: FirebaseAuth.instance.userChanges(),
+    return StreamBuilder<AppUser?>(
+      stream: UserStream.instance.stream,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Scaffold(body: Center(child: CircularProgressIndicator()));
-        } else if (snapshot.hasData) {
-          // only gets called once when auth state changes => manual check on verify screen necessary
-          bool isEmailVerified = snapshot.data!.isAnonymous || snapshot.data!.emailVerified;
-          return isEmailVerified ? const HomeScreen() : const VerifyScreen();
-        } else {
+        }
+        if (!snapshot.hasData) {
           return const WelcomeScreen();
         }
+        AppUser.instance = snapshot.data!;
+        if (AppUser.instance == AppUser.empty()) return const WelcomeScreen();
+        if (!AppUser.instance.verified) return const VerifyScreen();
+        CloudService.instance.loadData();
+        return const HomeScreen();
       },
     );
   }
