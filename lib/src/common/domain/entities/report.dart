8963e2d06eca97e8510616c2fca182189f7f3650
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-11-13 15:24:36 +0100

    [CHANGE] small class refactoring

--- lib/src/common/domain/entities/report.dart
+++ lib/src/common/domain/entities/report.dart
@@ -1,43 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:vocabualize/src/features/reports/domain/entities/report_type.dart';
 
-// TODO ARCHITECTURE: Check if Report class is fine
 class Report {
-  final bool done = false;
-  final Timestamp date = Timestamp.now();
+  final bool done;
+  final Timestamp date;
 
-  Report();
+  Report({
+    this.done = false,
+    date,
+  }) : date = date ?? Timestamp.now();
+
   factory Report.fromType({required ReportType type}) {
     switch (type) {
       case ReportType.bug:
-        return BugReport.empty();
+        return BugReport();
       case ReportType.translation:
-        return TranslationReport.empty();
+        return TranslationReport();
       default:
         return Report();
     }
-  }
-
-  String get formattedDate {
-    String year = date.toDate().year.toString().padLeft(4, "0");
-    String month = date.toDate().month.toString().padLeft(2, "0");
-    String day = date.toDate().day.toString().padLeft(2, "0");
-    return "$year$month$day";
-  }
-
-  String get formattedDateDetailed {
-    String hour = date.toDate().hour.toString().padLeft(2, "0");
-    String minute = date.toDate().minute.toString().padLeft(2, "0");
-    String second = date.toDate().second.toString().padLeft(2, "0");
-    String millisecond = date.toDate().millisecond.toString().padLeft(3, "0");
-    String microsecond = date.toDate().microsecond.toString().padLeft(3, "0");
-    return "$formattedDate-$hour-$minute-$second-$millisecond-$microsecond";
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'default': "default",
-    };
   }
 }
 
@@ -45,19 +26,10 @@ class BugReport extends Report {
   final String description;
   final String? data;
 
-  BugReport({required this.description, this.data});
-  BugReport.empty()
-      : description = "",
-        data = null;
-
-  @override
-  Map<String, dynamic> toJson() {
-    return {
-      "done": done,
-      "description": description,
-      "data": data,
-    };
-  }
+  BugReport({
+    this.description = "",
+    this.data,
+  });
 }
 
 class TranslationReport extends Report {
@@ -68,16 +40,10 @@ class TranslationReport extends Report {
   final String description;
 
   TranslationReport({
-    required this.source,
-    required this.target,
-    required this.sourceLanguageId,
-    required this.targetLanguageId,
+    this.source = "",
+    this.target = "",
+    this.sourceLanguageId = "",
+    this.targetLanguageId = "",
     this.description = "",
   });
-  TranslationReport.empty()
-      : source = "",
-        target = "",
-        description = "",
-        sourceLanguageId = "",
-        targetLanguageId = "";
 }
