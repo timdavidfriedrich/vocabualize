c09b739191512f1cd63ebafc01c5907ce1b0fd25
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-11-28 18:50:25 +0100

    [REMOVE] deprecated comment

--- lib/src/features/details/data/data_sources/draft_image_data_source.dart
+++ lib/src/features/details/data/data_sources/draft_image_data_source.dart
@@ -5,8 +5,9 @@ import 'package:log/log.dart';
 final draftImageDataSourceProvider = Provider((ref) => DraftImageDataSource());
 
 class DraftImageDataSource {
-  // TODO ARCHITECTURE: Remove dialog from DraftImageDataSource and move it to presentation layer
-  Future<XFile?> getImageFromCameraOrFiles({required ImageSource imageSource}) async {
+  Future<XFile?> getImageFromCameraOrFiles({
+    required ImageSource imageSource,
+  }) async {
     try {
       return await ImagePicker().pickImage(source: imageSource);
     } catch (e) {
