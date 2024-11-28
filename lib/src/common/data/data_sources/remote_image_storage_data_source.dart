b77825e721a133e8a1b3c4c1cc8a975d1035298a
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-11-28 18:50:35 +0100

    [REMOVE] unused data source

--- lib/src/common/data/data_sources/remote_image_storage_data_source.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final remoteImageStorageDataSourceProvider = Provider((ref) => RemoteImageStorageDataSource());
-
-// TODO ARCHITECTURE: Implement remote image storage data source
-class RemoteImageStorageDataSource {}
-  
