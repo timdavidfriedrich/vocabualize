437d5b748c85722454e018e46bc5b79f6e7531aa
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-11-29 07:21:33 +0100

    [CHANGE] refactor LanguagePickerScreen

--- lib/src/common/presentation/screens/language_picker_screen.dart
+++ lib/src/common/presentation/screens/language_picker_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:log/log.dart';
+import 'package:vocabualize/constants/dimensions.dart';
 import 'package:vocabualize/src/common/domain/entities/language.dart';
 import 'package:vocabualize/src/common/domain/use_cases/language/get_available_languages_use_case.dart';
 import 'package:vocabualize/src/common/presentation/extensions/context_extensions.dart';
@@ -11,64 +13,82 @@ class LanguagePickerScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final getAvailableLanguages = ref.watch(getAvailableLanguagesUseCaseProvider);
+    final getAvailableLanguages =
+        ref.watch(getAvailableLanguagesUseCaseProvider);
 
     void returnLanguage(Language language) {
       context.pop(language);
     }
 
-    ScrollController scrollController = ScrollController();
-
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
+          // TODO: Replace with arb
           title: const Text("Select Language"),
         ),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Center(
-            child: getAvailableLanguages.when(
-              loading: () {
-                return const Center(child: CircularProgressIndicator.adaptive());
-              },
-              error: (error, stackTrace) {
-                // TODO: Replace with error widget
-                return const Text("Error");
-              },
-              data: (List<Language> languages) {
-                return ListView(
-                  controller: scrollController,
-                  children: [
-                    const SizedBox(height: 16),
-                    GridView.count(
-                      controller: scrollController,
-                      shrinkWrap: true,
-                      crossAxisCount: 2,
-                      childAspectRatio: 2 / 1,
-                      children: List.generate(
-                        languages.length,
-                        (index) {
-                          return Card(
-                            child: ListTile(
-                              titleAlignment: ListTileTitleAlignment.center,
-                              onTap: () => returnLanguage(languages.elementAt(index)),
-                              // enabled: selectedLanguage != Provider.of<SettingsProvider>(context, listen: false).sourceLanguage,
-                              title: Text(
-                                languages.elementAt(index).name,
-                                // textAlign: TextAlign.center,
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    const SizedBox(height: 32),
-                  ],
-                );
-              },
-            ),
+          padding: const EdgeInsets.symmetric(
+            horizontal: Dimensions.mediumSpacing,
+          ),
+          child: getAvailableLanguages.when(
+            loading: () {
+              return const Center(
+                child: CircularProgressIndicator.adaptive(),
+              );
+            },
+            error: (error, stackTrace) {
+              Log.error(
+                "Error LanguagePickerScreen: $error",
+                exception: stackTrace,
+              );
+              // TODO: Replace with error widget
+              return const Text("Error LanguagePickerScreen");
+            },
+            data: (List<Language> languages) {
+              return GridView.builder(
+                physics: const BouncingScrollPhysics(),
+                padding: const EdgeInsets.only(
+                  top: Dimensions.mediumSpacing,
+                  bottom: Dimensions.scrollEndSpacing,
+                ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: languages.length == 1 ? 1 : 2,
+                  childAspectRatio: 2 / 1,
+                ),
+                itemCount: languages.length,
+                itemBuilder: (context, index) {
+                  return _LanguageCard(
+                    language: languages.elementAt(index),
+                    onClick: returnLanguage,
+                  );
+                },
+              );
+            },
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _LanguageCard extends StatelessWidget {
+  final Language language;
+  final void Function(Language) onClick;
+  const _LanguageCard({
+    required this.language,
+    required this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        // ? enabled: languages.elementAt(index) != selectedLanguage,
+        onTap: () {
+          onClick(language);
+        },
+        titleAlignment: ListTileTitleAlignment.center,
+        title: Text(language.name),
       ),
     );
   }
